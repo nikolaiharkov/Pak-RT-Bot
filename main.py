1063524a80cb520e55fa28796dce0b92efbd9061
Nikolai Harkov <74813988+nikolaiharkov@users.noreply.github.com>
2022-05-21 23:09:09 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from discord import client
 import os
 from discord.ext import commands
 
+activity = discord.Activity(type=discord.ActivityType.listening, name=">help | for more info")
+
 # bot prefix
 intents=discord.Intents.default()
 intents.members=True
-client = commands.Bot(command_prefix=">", intents=intents)
+client = commands.Bot(command_prefix=">", intents=intents, activity=activity, status=discord.Status.online)
+
 
 # loading all the cogs
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py") and not filename.startswith("_"):
         client.load_extension(f"cogs.{filename[:-3]}")
+    
 
-token="OTU4NjY4MDc4ODc3NjcxNDU0.YkQrOQ.1XrlTTNMYupoNr0lv0MAFlYuSio"
+token="OTU4NjY4MDc4ODc3NjcxNDU0.YkQrOQ._POv5ggXb0aQJuUjIzUwZPTMZvQ"
 client.run(token)
